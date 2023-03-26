bf8c15203e798d97972c244433cef9ac63458791
Froxy <128308315+FroxyGood@users.noreply.github.com>
2023-03-26 15:37:45 +0300

    Add files via upload

---
+++ Basit Matematik İşlemleri.py
@@ -0,0 +1,19 @@
+A=int(input("1. sayıyı giriniz"))
+B=int(input("2. sayıyı giriniz"))
+import math
+TOPLAM = A+B
+ÇARPIM = A*B
+BÖLÜM = A/B
+BÖLÜM2 = A//B
+ASAYISININKARESİ = A*A
+BSAYISININKaresi = B*B
+ASAYISININKAREKÖKDEĞERİ= math.sqrt(A)
+BSAYISININKAREKÖKDEĞERİ = math.sqrt(B)
+print ("Toplam:",TOPLAM)
+print ("Çarpım:",ÇARPIM)
+print ("Bölüm:",BÖLÜM)
+print ("Kalansız Bölüm:",BÖLÜM2)
+print ("İlk sayının karesi:",ASAYISININKARESİ)
+print ("İkinci sayının karesi:",BSAYISININKaresi)
+print ("İlk sayının karekökü:",ASAYISININKAREKÖKDEĞERİ)
+print ("İkinci sayının karekökü:",BSAYISININKAREKÖKDEĞERİ)
